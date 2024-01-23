b10f73b4be2f0be8bfa087f68ccf7f1c5d5f3ab4
Simon R <122458621+skillhunter333@users.noreply.github.com>
2024-01-23 10:30:31 +0100

    introduced login via JWT stored in Cookie

--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -1,7 +1,40 @@
+const dotenv = require("dotenv")
+dotenv.config();
+const UserCollection = require("../models/userSchema");
+const ErrorStatus = require("../utils/errorStatus");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 
 const userController = {
-    login: (req, res) => {
-        res.status(200).json({ message: "Login test controller" });
+    
+        login: async (req, res, next) => {
+    
+    try {
+    const { username, password } = req.body;
+    if (!username || !password) throw new ErrorStatus("Fehlender Benutzername oder Passwort", 400);
+    const foundUser = await UserCollection.findOne({ username }).select(
+      "+password"
+    );
+    if (!foundUser) throw new ErrorStatus("Benutzer nicht gefunden", 404);
+
+    const compare = await bcrypt.compare(password, foundUser.password);
+    if (!compare) throw new ErrorStatus("Passwort stimmt nicht überein", 401);
+
+    const token = jwt.sign({ _id: foundUser._id }, process.env.JWT_SECRET, { expiresIn: '12h' });
+    
+
+    return res
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 12, //Cookie verfällt nach 12h 
+        sameSite: "none",
+        secure: true,
+      })
+      .sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
     },
 };
 
