02a201ffbcf4e8ee8eb41bf67f59df20c17629e4
Simon R <122458621+skillhunter333@users.noreply.github.com>
2024-01-23 13:35:39 +0100

    added CORS

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Router = require('./routes/routes.js'); 
 const connectDB = require('./db/connectDB.js');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +9,10 @@ const port = process.env.PORT || 3000;
 connectDB();
 
 app.use(express.json());
+app.use(cors({
+  origin: [process.env.FRONTEND_DEV, process.env.FRONTEND_DEPLOYED],
+  credentials: true 
+}));
 app.use(cookieParser());
 
 app.use('/', Router);
